beaf4323e33316206bd08a5f5ff39237b4d7969f
Patricio <ratcliffepatricio321@gmail.com>
2025-09-18 19:53:05 -0300

    Tabs iniciales

--- app/(tabs)/discover.tsx
+++ app/(tabs)/discover.tsx
@@ -1,6 +1,6 @@
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function DiscoverIndex() {
   return (
     <View
       style={{
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Este va a ser el Feed.</Text>
+      <Text>Este va a ser el Discover.</Text>
     </View>
   );
 }
