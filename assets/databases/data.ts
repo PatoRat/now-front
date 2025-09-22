15abe3cc202e9bbe96462f9a1ce5a93255213744
Patricio <ratcliffepatricio321@gmail.com>
2025-09-22 01:43:43 -0300

    Probando la presentacion de contenido en el feed 2.0

--- assets/databases/data.ts
+++ assets/databases/data.ts
@@ -1,11 +1,28 @@
 import { PostType } from "@/scripts/types";
 
-const DATA: [PostType] = [
+const DATA: PostType[] = [
     {
         id: "1",
         titulo: "Suecia",
         descripcion: "Viajecito a Suecia",
         imagenes: []
+    },
+    {
+        id: "2",
+        titulo: "Sample1",
+        descripcion: "SampleText1",
+        imagenes: [
+            require("@/assets/images/favicon.png")
+        ]
+    },
+    {
+        id: "3",
+        titulo: "Sample2",
+        descripcion: "SampleText2",
+        imagenes: [
+            require("@/assets/images/icon.png"),
+            require("@/assets/images/react-logo.png")
+        ]
     }
 ];
 
